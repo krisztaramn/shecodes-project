73faf6da2904bf18aa958139503f68ee072bea1f
krisztaramn <60409575+krisztaramn@users.noreply.github.com>
2020-04-19 21:19:37 +0100

    Added GitHub link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Test</p>
+        <p className="open-source">
+          <a
+            href="https://github.com/krisztaramn/shecodes-project"
+            target="_blank"
+            className="github-link"
+          >
+            {" "}
+            Open-source cod
+          </a>{" "}
+          by Krisztina Ramn
+        </p>
       </header>
     </div>
   );
